393fe6e3ec632100c5310b79fc0ebce9a25dec0a
Peter Bardon <pbard42@uw.edu>
2016-04-04 23:32:40 -0700

    got redirection fixed and set up to use single ec2 instance

--- src/server.js
+++ src/server.js
@@ -18,19 +18,21 @@
         console.log(key);
         urlDb.getUrl(key).then(function(urlObject){
            console.log('promise resolved, checking urlObj:');
-           console.log(urlObject); 
+           console.log(urlObject);
            if (urlObject) {
                 console.log('inside urlobj if');
                 var scheme = '';
-                if (!urlObject.url.slice(4) === 'http') {
+                if (urlObject.url.slice(0, 4) !== 'http') {
                     scheme = 'http://';
                 }
                 var redirectUrl = scheme + urlObject.url;
+                console.log('redirect url is:');
+                console.log(redirectUrl);
                 console.log('about to wrtie head');
                 response.writeHead(301, {
                     'Location' :redirectUrl
                 });
-                console.log('about to resolve promise2...')
+                console.log('about to resolve promise2...');
                 return deferred.resolve({ response: response, data: urlObject});
             }
             return deferred.reject(new Error(
